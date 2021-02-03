d3b7fa8205ec7a36d1d72b10a558796e2f111cb2
Amankaium <kaium@list.ru>
2021-02-03 19:25:38 +0600

    created book addition

--- app.py
+++ app.py
@@ -46,4 +46,12 @@ def authors():
 def add():
     f = request.form
     # print(f["author"], f["book"])
+    excel = load_workbook("tales.xlsx")
+    page = excel["Лист1"]
+    last = len(page["A"]) + 1
+    page[f"A{last}"] = f["book"]
+    page[f"B{last}"] = f["author"]
+    excel.save("tales.xlsx")
     return "форма получена"
+
+
